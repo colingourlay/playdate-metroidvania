4022c3a43c7204ac00c81c13132bbccd300aa33c
Colin Gourlay <colin@colin-gourlay.com>
2023-02-16 20:01:47 +1000

    Add assets. Add level

---
+++ src/scripts/GameScene.lua
@@ -0,0 +1,34 @@
+local gfx <const> = playdate.graphics
+local ldtk <const> = LDtk
+
+ldtk.load("levels/world.ldtk", false)
+
+class('GameScene').extends()
+
+function GameScene:init()
+  self:goToLevel("Level_0")
+end
+
+function GameScene:goToLevel(level_name)
+  gfx.sprite.removeAll()
+
+  for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
+    if layer.tiles then
+      local tilemap = ldtk.create_tilemap(level_name, layer_name)
+      local layerSprite = gfx.sprite.new()
+      
+      layerSprite:setTilemap(tilemap)
+      layerSprite:setCenter(0, 0)
+      layerSprite:moveTo(0, 0)
+      layerSprite:setZIndex(layer.zIndex)
+      layerSprite:add()
+
+      local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
+
+      if emptyTiles then
+        gfx.sprite.addWallSprites(tilemap, emptyTiles)
+      end
+    end
+  end
+end
+
